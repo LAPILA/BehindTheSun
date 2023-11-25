46349139513d3e1f5697667e8184dd7f2adf51e5
Gyo Chan -Jin <jinkai4868@gmail.com>
2023-11-25 15:30:06 +0900

    Map Update

---
+++ BehindTheSun/Assets/Test/Scripts/Village/GoVillage.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoVillage : MonoBehaviour
+{
+    private CharacterController2D Player;
+
+    private void Start()
+    {
+        Player = FindObjectOfType<CharacterController2D>();
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "dummy" && Input.GetKeyDown(KeyCode.C))
+        {
+            SceneManager.LoadScene("Village");
+
+        }
+    }
+
+}
